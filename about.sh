9ccf09e6688978a3f180c211271e4e86f5e70953
chiddy80 <rashidkamote95@gmail.com>
2025-11-28 12:39:23 +0300

    Create about.sh

---
+++ about.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+clear
+echo -e "================================================="
+echo -e "#     Premium Auto Script By Mr Chiddy (EsimFreeGB)      #"
+echo -e "================================================="
+echo -e "# For Debian 10 64 bit                          #"
+echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #"
+echo -e "# For VPS with KVM and VMWare virtualization    #"
+echo -e "# Build Up By Akbar Maulana                     #"
+echo -e "================================================="
+echo -e "#  Special Thanks To                                     #"
+echo -e "================================================="
+echo -e "# Allah SWT                                     #"
+echo -e "# My Family                                     #"
+echo -e "# Sakura                                       #"
+echo -e "# ispismyself                                       #"
+echo -e "================================================="
